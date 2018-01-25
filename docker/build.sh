56b04506eefe60ef2c54cff5e2b056af53fd8bad
fcastelain <fcastelain@excilys.com>
2018-01-25 11:27:58 +0100

    [FEAT] - Mount two volumes (manage log and apps outside of the mule container.

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,5 @@
+# now we create the network of production
+docker network create --subnet=172.19.0.0/16 prodNetwork
+
 # Build the image of the muleInDocker/mule
 docker build ./mule/ --tag muleindocker/mule:latest
--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,2 @@
-# now we create the network of production
-#docker network create --subnet=172.19.0.0/16 prodNetwork
-
 # add the mysql prod to the network
-docker run --net prodNetwork --ip 172.19.0.5 -dti --name muleContainer muleindocker/mule:latest
+docker run --net prodNetwork --ip 172.19.0.5 -v c:/Users/salon/Documents/project/muleInDocker/docker/mule/logs:/home/muleindocker/mule/logs -v c:/Users/salon/Documents/project/muleInDocker/docker/mule/apps:/home/muleindocker/mule/apps -dti --name muleContainer muleindocker/mule:latest
