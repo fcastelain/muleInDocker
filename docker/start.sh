c1be55bb32b11e5b51243f9f7b5dfc80e978e416
fcastelain <fcastelain@excilys.com>
2018-01-25 09:47:23 +0100

    [FEAT] - Use openjdk images to avoid problem with alpine.

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,5 @@
 # now we create the network of production
-docker network create --subnet=172.19.0.0/16 prodNetwork
+#docker network create --subnet=172.19.0.0/16 prodNetwork
 
 # add the mysql prod to the network
 docker run --net prodNetwork --ip 172.19.0.5 -dti --name muleContainer muleindocker/mule:latest
