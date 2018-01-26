36f8fcb3c6977bac5adf7450f5985cd43b05860a
fcastelain <fcastelain@excilys.com>
2018-01-26 12:45:50 +0100

    [REFACTO] - Need to deplace source code of the project inside the maven repository.

--- clean.sh
+++ clean.sh
@@ -6,9 +6,13 @@ docker network rm muleNetwork
 
 # remove the container
 docker rm muleContainer
+docker rm mavenContainer
+docker rm newmanContainer
 
 # remove the images
 docker rmi muleindocker/mule:latest
+docker rmi muleindocker/maven:latest
+docker rmi muleindocker/newman:latest
 
 # clean all the volume
 docker volume rm $(docker volume ls -qf dangling=true)
