ef402635ed8652c66f5848eebe68f4574b01f784
fcastelain <fcastelain@excilys.com>
2018-01-26 13:23:30 +0100

    [REFACTO] - Change the path of the mount volume.

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 # add the mysql prod to the network
-docker run --net muleNetwork --ip 172.19.0.2 -v c:/Users/salon/Documents/project/muleInDocker/docker/mule/logs:/home/muleindocker/mule/logs -v c:/Users/salon/Documents/project/muleInDocker/docker/mule/apps:/home/muleindocker/mule/apps -dti --name muleContainer muleindocker/mule:latest
+docker run --net muleNetwork --ip 172.19.0.2 -v c:/Users/salon/Documents/project/muleInDocker/mule/logs:/home/muleindocker/mule/logs -v c:/Users/salon/Documents/project/muleInDocker/mule/apps:/home/muleindocker/mule/apps -dti --name muleContainer muleindocker/mule:latest
