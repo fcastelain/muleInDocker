44a3015ba810cfdba9eb3ce62992242ffd2ad23d
fcastelain <fcastelain@excilys.com>
2018-01-26 15:52:11 +0100

    [FEAT] - Add a script to run the nrg test with newman.

--- testNrg.sh
+++ testNrg.sh
@@ -1,2 +1,2 @@
 # run nrg test on the instance of the mule soft
-docker run --net muleNetwork -t muleindocker/newman:latest --collection="collection/muleInDocker-nrg.postman_collection.json" --environment="environment/DEV.postman_environment.json" --html="newman-results.html" --name newmanContainer
+docker run --name newmanContainer --net muleNetwork --ip 172.19.0.4 -t muleindocker/newman:latest --collection="collection/muleInDocker-nrg.postman_collection.json" --environment="environment/DEV.postman_environment.json" --html="newman-results.html"
