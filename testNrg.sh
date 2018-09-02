e8e941a59850146b4f2345c84e3e7439cf3b256b
fcastelain <fcastelain@excilys.com>
2018-09-02 18:03:08 +0200

    [FEAT] - Remove the docker after the nrg tests.

--- testNrg.sh
+++ testNrg.sh
@@ -1,2 +1,5 @@
 # run nrg test on the instance of the mule soft
 docker run --name newmanContainer --net muleNetwork --ip 172.19.0.4 -t muleindocker/newman:latest --collection="collection/muleInDocker-nrg.postman_collection.json" --environment="environment/DEV.postman_environment.json" --html="newman-results.html"
+
+# remove the docker after the test is end
+docker rm newmanContainer
